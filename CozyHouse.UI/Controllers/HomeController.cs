302621feda9f9ed293477be6553c9ff637030a6c
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-03-11 17:41:36 +0200

    Clean Architecture Implemented

--- CozyHouse.UI/Controllers/HomeController.cs
+++ CozyHouse.UI/Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-using CozyHouse.UI.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace CozyHouse.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -21,12 +12,6 @@ namespace CozyHouse.UI.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
