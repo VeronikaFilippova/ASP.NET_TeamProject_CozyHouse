4f1750e5f92e4b8e7f0f262068c62d2a50f7d39f
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-03-11 17:42:21 +0200

    Revert "Clean Architecture Implemented"
    This reverts commit 302621feda9f9ed293477be6553c9ff637030a6c.

--- CozyHouse.UI/Controllers/HomeController.cs
+++ CozyHouse.UI/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
+using CozyHouse.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace CozyHouse.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,12 @@ namespace CozyHouse.UI.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
