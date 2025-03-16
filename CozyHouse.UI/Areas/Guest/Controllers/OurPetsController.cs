e6f3a87c61923a11f292aa2b9ec79abacc483ec0
lokshuna <lilivolos2005@gmail.com>
2025-03-16 15:48:51 +0200

    Add UI

---
+++ CozyHouse.UI/Areas/Guest/Controllers/OurPetsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CozyHouse.UI.Areas.Guest.Controllers
+{
+    [Area("Guest")]
+    [AllowAnonymous]
+    public class OurPetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
