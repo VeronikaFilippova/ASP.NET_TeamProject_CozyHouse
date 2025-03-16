6223495d3def070e3851e2689b1836123fa89246
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-03-16 10:51:14 +0200

    Logout implemented

--- CozyHouse.UI/Areas/Manager/Controllers/ManagerHomeController.cs
+++ CozyHouse.UI/Areas/Manager/Controllers/ManagerHomeController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozyHouse.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CozyHouse.UI.Areas.Manager.Controllers
 {
     [Area("Manager")]
     public class ManagerHomeController : Controller
     {
+        SignInManager<ApplicationUser> _signInManager;
+        public ManagerHomeController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> LogoutCommand()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "GuestHome", new { area = "Guest" });
         }
     }
 }
--- CozyHouse.UI/Areas/User/Controllers/UserHomeController.cs
+++ CozyHouse.UI/Areas/User/Controllers/UserHomeController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozyHouse.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CozyHouse.UI.Areas.User.Controllers
 {
     [Area("User")]
     public class UserHomeController : Controller
     {
+        SignInManager<ApplicationUser> _signInManager;
+        public UserHomeController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> LogoutCommand()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "GuestHome", new { area = "Guest" });
         }
     }
 }
