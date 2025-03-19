908cd5a7ba1fe6416fd2a7a67255643548e9bd22
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-03-19 12:44:48 +0200

    User area SeePets, SeeMore implemented

---
+++ CozyHouse.UI/Areas/User/Controllers/AdoptionController.cs
@@ -0,0 +1,32 @@
+﻿using CozyHouse.Core.Domain.Entities;
+using CozyHouse.Core.RepositoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CozyHouse.UI.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize(Roles = "User")]
+    public class AdoptionController : Controller
+    {
+        public AdoptionController()
+        {
+
+        }
+        [HttpPost]
+        public IActionResult Adopt(Guid id)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value!;
+            Request request = new Request()
+            {
+                ListingId = id,
+                UserId = Guid.Parse(userId),
+                IsClosed = false,
+            };
+            // Add Request
+
+            return RedirectToAction("Index", "UserHome");
+        }
+    }
+}
---
+++ CozyHouse.UI/Areas/User/Controllers/PetsController.cs
@@ -0,0 +1,31 @@
+﻿using CozyHouse.Core.Domain.Entities;
+using CozyHouse.Core.RepositoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CozyHouse.UI.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize(Roles = "User")]
+    public class PetsController : Controller
+    {
+        IListingRepository _listingRepository;
+        public PetsController(IListingRepository listingRepository)
+        {
+            _listingRepository = listingRepository;
+        }
+        public IActionResult ShelterPets()
+        {
+            return View(_listingRepository.GetAll());
+        }
+        // TODO: Create table for user listings. Create Repository, Implement this method.
+        public IActionResult UserPets()
+        {
+            return View();
+        }
+        public IActionResult SeeMore(Guid id)
+        {
+            return View(_listingRepository.GetListing(id));
+        }
+    }
+}
