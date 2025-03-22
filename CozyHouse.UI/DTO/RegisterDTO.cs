41daf45986cc4d8d292c995c23c5fc47bf563896
valeriaponomariova <129272436+valeriaponomariova@users.noreply.github.com>
2025-03-22 19:34:24 +0800

    Оновила структури RegisterDTO.cs та LoginDTO.cs  , додала нові кнопки в index.cshtml, змінила стилі в authorization.css

--- CozyHouse.UI/DTO/RegisterDTO.cs
+++ CozyHouse.UI/DTO/RegisterDTO.cs
@@ -7,5 +7,8 @@
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
+        public int Age { get; set; } // Властивість Age
+        public string Location { get; set; } // Властивість Location
+
     }
 }
