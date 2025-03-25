f7e19799bc033c6e8311e3bd2d4e3f1e1a419c2e
Вероніка Філіппова <veronika.filippova@lnu.edu.ua>
2025-03-25 14:01:30 +0200

    Added a new pet

--- CozyHouse.Infrastructure/Repositories/UserRepository.cs
+++ CozyHouse.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using CozyHouse.Core.Domain.IdentityEntities;
+﻿using CozyHouse.Core.Domain.Entities;
+using CozyHouse.Core.Domain.IdentityEntities;
 using CozyHouse.Core.RepositoryInterfaces;
 using CozyHouse.Infrastructure.Database;
 
@@ -11,6 +12,57 @@ namespace CozyHouse.Infrastructure.Repositories
         {
             db = databse;
         }
+
+        public ApplicationUser? GetUser(Guid userId)
+        {
+            return db.Users.FirstOrDefault(user => user.Id == userId);
+        }
+
+        public bool AddUser(ApplicationUser user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool DeleteUser(Guid userId)
+        {
+            try
+            {
+                ApplicationUser userToDelete = db.Users.First(user => user.Id == userId);
+                db.Users.Remove(userToDelete);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+
+        }
+
+        public bool UpdateUser(ApplicationUser user)
+        {
+            try
+            {
+                ApplicationUser userToUpdate = db.Users.First(u => u.Id == user.Id);
+
+                userToUpdate.PersonName = user.PersonName;
+
+                db.Users.Update(userToUpdate);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public List<ApplicationUser> GetAllUsers()
+        {
+            return db.Users.ToList();
+
+        }
+
         public ApplicationUser? GetByUserName(string? userName)
         {
             if (userName == null) return null;
