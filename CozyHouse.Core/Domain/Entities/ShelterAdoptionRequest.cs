b768e87df7c28758299c511f752e32b618d421d5
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-04-06 13:29:28 +0300

    Unnessesary data anotations removed

--- CozyHouse.Core/Domain/Entities/ShelterAdoptionRequest.cs
+++ CozyHouse.Core/Domain/Entities/ShelterAdoptionRequest.cs
@@ -1,18 +1,15 @@
 ﻿using CozyHouse.Core.Domain.Entities.BaseEntities;
 using CozyHouse.Core.Domain.IdentityEntities;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CozyHouse.Core.Domain.Entities
 {
     public class ShelterAdoptionRequest : AdoptionRequestBase
     {
-        [Required]
         [ForeignKey(nameof(Adopter))]
         public Guid AdopterId { get; set; }
         public ApplicationUser Adopter { get; set; } = null!;
 
-        [Required]
         [ForeignKey(nameof(PetPublication))]
         public Guid PetPublicationId { get; set; }
         public ShelterPetPublication PetPublication { get; set; } = null!;
--- CozyHouse.Core/Domain/Entities/UserAdoptionRequest.cs
+++ CozyHouse.Core/Domain/Entities/UserAdoptionRequest.cs
@@ -1,26 +1,21 @@
 ﻿using CozyHouse.Core.Domain.Entities.BaseEntities;
 using CozyHouse.Core.Domain.IdentityEntities;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace CozyHouse.Core.Domain.Entities
 {
     public class UserAdoptionRequest : AdoptionRequestBase
     {
-        [Required]
         [ForeignKey(nameof(Adopter))]
         public Guid AdopterId { get; set; }
         public ApplicationUser Adopter { get; set; } = null!;
 
-        [Required]
         [ForeignKey(nameof(Owner))]
         public Guid OwnerId { get; set; }
         public ApplicationUser Owner { get; set; } = null!;
 
-        [Required]
         [ForeignKey(nameof(PetPublication))]
         public Guid PetPublicationId { get; set; }
         public UserPetPublication PetPublication { get; set; } = null!;
-
     }
 }
