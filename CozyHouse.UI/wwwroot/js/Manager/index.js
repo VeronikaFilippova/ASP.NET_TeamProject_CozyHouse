de1147a91efad90b5a34647b83bd9533e1533adf
Oleh <67824688+OlehChyzhov@users.noreply.github.com>
2025-04-20 22:01:06 +0300

    Additional information with nyan-cat implemented

--- CozyHouse.UI/wwwroot/js/Manager/index.js
+++ CozyHouse.UI/wwwroot/js/Manager/index.js
@@ -15,3 +15,23 @@ $(document).ready(function () {
         lengthChange: false
     });
 });
+
+function showDetails(row) {
+    const user = JSON.parse(row.getAttribute("data-user"));
+    let userDetailsHtml =
+    `<div class="user-details">
+        <h3>User ID: ${user.id}</h3>
+        <h3>Person Name: ${user.personName}</h3>
+        <h3>Phone Number: ${user.phoneNumber}</h3>
+        <h3>Phone Number Confirmed: ${user.phoneNumberConfirmed}</h3>
+    </div>`;
+    Swal.fire({
+        title: 'User Details',
+        html: userDetailsHtml,
+        icon: 'info',
+        confirmButtonText: 'Got it!',
+        showCloseButton: true,
+        width: '40rem',
+        backdrop: `rgba(0, 0, 123, 0.4) url("/img/nyan-cat.gif") left top no-repeat`
+    });
+}
